36adda7f0d6e0b54024089406508a904ab6c099a
Daniel Röhers Moura <rohersmoura@gmail.com>
2015-06-23 15:53:40 -0300

    heroku nojento 2

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 'use strict';
 
 var scheduler = require('node-schedule');
-var exec = require('child_process').exec;
-var http = require('http');
 var seriesJob = require('./jobs/series');
 var moviesJob = require('./jobs/movies');
 var log = require('./utils/logger')('main', 'Main');
@@ -24,23 +22,8 @@ var _runMoviesJob = function () {
     }
 };
 
-if (process.env.NODE_ENV !== 'development') {
-    var port = Number(process.env.PORT || 3000);
-    http.createServer(function (req, res) {
-  
-        // Executes every 1 minute(s)
-        var _seriesSchedule = scheduler.scheduleJob('*/1 * * * *', _runSeriesJob);
+// Executes every 1 minute(s)
+var _seriesSchedule = scheduler.scheduleJob('*/1 * * * *', _runSeriesJob);
 
-        // Executes every 1 minute(s)
-        var _moviesSchedule = scheduler.scheduleJob('*/1 * * * *', _runMoviesJob);
-
-    }).listen(port, '127.0.0.1');
-
-    log.info('Server running at http://127.0.0.1:' + port);    
-} else {
-    // Executes every 1 minute(s)
-    var _seriesSchedule = scheduler.scheduleJob('*/1 * * * *', _runSeriesJob);
-
-    // Executes every 1 minute(s)
-    var _moviesSchedule = scheduler.scheduleJob('*/1 * * * *', _runMoviesJob);
-}
+// Executes every 1 minute(s)
+var _moviesSchedule = scheduler.scheduleJob('*/1 * * * *', _runMoviesJob);
